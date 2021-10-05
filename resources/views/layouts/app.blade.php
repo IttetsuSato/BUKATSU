9046694c35d0c8c69a84424db1700f934c007866
ittetsu51 <ittetsusato@gmail.com>
2021-10-05 18:14:31 +0900

    pdfをpublicに移動

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,13 +37,13 @@
             <div class="footer">
               <div class="bukatsu-bg-blue py-6 px-4 sm:px-6 lg:px-8">
                 <div class="mx-auto bukatsu-text-white">
-                  <a href="{{ asset('storage/pdf/BUKATSU会社概要.pdf') }}" target="_blank">
+                  <a href="{{ asset('pdf/BUKATSU会社概要.pdf') }}" target="_blank">
                     <p class="font-bold text-xl my-4">会社案内</p>
                   </a>
-                  <a href="{{ asset('storage/pdf/BUKATSU利用規約.pdf') }}" target="_blank">
+                  <a href="{{ asset('pdf/BUKATSU利用規約.pdf') }}" target="_blank">
                     <p class="font-bold text-xl  my-4">利用規約</p>
                   </a>
-                  <a href="{{ asset('storage/pdf/BUKATSUプライバシーポリシー.pdf') }}" target="_blank">
+                  <a href="{{ asset('pdf/BUKATSUプライバシーポリシー.pdf') }}" target="_blank">
                     <p class="font-bold text-xl  my-4">プライバシーポリシー</p>
                   </a>
                   <a href="#">

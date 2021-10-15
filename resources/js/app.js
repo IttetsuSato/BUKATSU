4430674fd4506db2d64013d1aa2eca628378a010
ittetsu51 <ittetsusato@gmail.com>
2021-10-15 10:35:51 +0900

    マイページで画像登録

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,4 @@
 require('./bootstrap');
-// require('./mycrop');
-
 require('alpinejs');
 
 // 数字がたくさんあるoptionタグを作る関数
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,8 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> --}}
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        {{-- <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> --}}
         <script src="https://kit.fontawesome.com/137e363532.js" crossorigin="anonymous" defer></script>
         {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script> --}}
         {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" defer></script> --}}
